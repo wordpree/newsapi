44071ad98dcc766760b0befab7349cdae2cf050f
jun <wanghaijun04@gmail.com>
2019-03-17 00:19:26 +1000

    built up single news component

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-const News =()=>{}; 
+const News =({articleInfo})=>(
+  <div className="col-3">
+    <div className="card" style={ {width: '18rem'} }>
+      <img className="card-img-top" src={articleInfo.urlToImage} alt={articleInfo.title} />
+      <div className="card-body">
+        <h5 class="card-title">{articleInfo.title}</h5>
+        <p className="card-text">{articleInfo.description}</p>
+        <a href={articleInfo.url} className="btn btn-primary">Full Article</a>
+      </div>
+    </div>
+  </div>
+);
 
 
 export default News;
