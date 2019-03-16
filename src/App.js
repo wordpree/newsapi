b6388199caffde440d20ee25abf397fbcde13f2d
jun <wanghaijun04@gmail.com>
2019-03-17 00:18:24 +1000

    added articles state as NewsLists props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import NewsLists from './components/NewsLists/NewsLists';
+import { newsDataFetch } from './util/NewsDataFetch';
+
+const info = [1,2,3,4,5,6,7,8];
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      articles:[]
+    }
+  }
+  componentDidMount(){
+    newsDataFetch().then(data=>this.setState({
+      articles:data
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <NewsLists />
+        <NewsLists newsfeed={this.state.articles} />
       </div>
     );
   }
