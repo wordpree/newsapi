35a8faced49715279615bcf550554d4f6d4c7501
jun <wanghaijun04@gmail.com>
2019-03-17 17:37:36 +1000

    added key props for each News children

--- src/components/NewsLists/NewsLists.js
+++ src/components/NewsLists/NewsLists.js
@@ -1,9 +1,10 @@
 import React,{ Component } from 'react';
 import News from '../News/News';
+import './NewsLists.scss';
 class NewsLists extends Component {
   render(){
 
-    let news = this.props.newsfeed.map(item=><News articleInfo={item}/>);
+    let news = this.props.newsfeed.map(item=><News articleInfo={item} key={item.url} />);
     return (
       <div className='container'>
         <div className='row'>
