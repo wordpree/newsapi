b0cbda18e6e46f989c9f8136e0fdffb5a0db4019
jun <wanghaijun04@gmail.com>
2019-03-20 17:23:42 +1000

    added data-* for differe component call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import NewsLists from './components/NewsLists/NewsLists';
-import SelectLists from './components/SelectLists/SelectLists';
 import SideNewsLists from './components/SideNewsLists/SideNewsLists';
 
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={type:'top-headlines'};
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(e){
+
+    this.setState({
+      type:e.target.getAttribute('data-state-value')
+    });
+  }
+
 
   render() {
 
     return (
       <div className="App">
-        <SelectLists />
-        <NewsLists />
-        <SideNewsLists />
+        <nav>
+          <ul>
+            <li data-state-value='top-headlines' onClick={this.handleOnClick}>Hit Top</li>
+            <span></span>
+            <li data-state-value='everything' onClick={this.handleOnClick}>Everything</li>
+          </ul>
+        </nav>
+        <NewsLists type={this.state.type} key={this.state.type} />
       </div>
     );
   }
+
 }
 
 export default App;
