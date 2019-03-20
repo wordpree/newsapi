3733ae5e52ca8c7c40538925e63ee5a1631a0475
jun <wanghaijun04@gmail.com>
2019-03-20 22:28:48 +1000

    built up fetch remote data functionality && set up state && to avoid same key,inserting in with map index

--- src/components/NewsLists/NewsLists.js
+++ src/components/NewsLists/NewsLists.js
@@ -2,12 +2,31 @@ import React,{ Component } from 'react';
 import News from '../News/News';
 import { Svg } from '../Svg/Svg';
 import './NewsLists.scss';
+import { newsDataFetch } from '../../util/NewsDataFetch';
+
 class NewsLists extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      articles:[],
+      loading:true
+    }
+  }
+
+  componentDidMount(){
+
+    newsDataFetch(this.props.type).then(data=>this.setState({
+        articles:data,
+        loading:false
+        })
+      );
+
+  }
   render(){
 
-    let news = this.props.loading ? <Svg /> : this.props.newsfeed.map(item=><News articleInfo={item} key={item.url} />);
+    let news = this.state.loading ? <Svg /> : this.state.articles.map((item,index)=><News articleInfo={item} key={item.url+index} />);
     return (
-      <div className='container'>
+      <div className='articles-container'>
         <div className='row'>
           {news}
         </div>
