19b37ac6f8763a986fe9503c6b1a58b098febcb2
jun <wanghaijun04@gmail.com>
2019-03-20 17:26:53 +1000

    added new attribute for opening new table

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,11 +5,11 @@ const News =({articleInfo})=>(
   <div className="card-container">
     <div className="card-image" style={{backgroundImage:`url(${articleInfo.urlToImage})`}}>
       <div className="card-body">
-        <a href={articleInfo.url} >
+        <a href={articleInfo.url} target='_blanket' >
           <h5 className="card-title">{articleInfo.title}</h5>
           <button className="btn">Full Article</button>
         </a>
-      </div>    
+      </div>
     </div>
   </div>
 );
