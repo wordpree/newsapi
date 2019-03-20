fd8ea8a61e8bebc5d98d8249a77149a746af4724
jun <wanghaijun04@gmail.com>
2019-03-20 22:24:45 +1000

    added functionality for different reponse data structure

--- src/util/NewsDataFetch.js
+++ src/util/NewsDataFetch.js
@@ -1,7 +1,16 @@
-const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=6352c20ad9204ab181b8a82ac99d0299";
 
-export let newsDataFetch = ()=>{
+
+export let newsDataFetch = (searchType)=>{
+  
+  let url = `https://newsapi.org/v2/${searchType}?q=apple&apiKey=6352c20ad9204ab181b8a82ac99d0299`;
   return fetch(url).then(response=>response.json())
-                   .then(resJson => resJson.articles );
-                   //.catch(err=>console.log(err));
+                   .then(resJson => {
+                     if(resJson.articles)
+                     {
+                       return resJson.articles;
+                     }else if (resJson.sources) {
+                       return resJson.sources;
+                     }
+                   })
+                   .catch(err=>console.log(err));
   }
