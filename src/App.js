82a0f17afc144758a976a2fd785523fe7b63ac72
jun <wanghaijun04@gmail.com>
2019-03-20 11:45:42 +1000

    refactored,components state sorted and added new component selectlists for different search rules

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import NewsLists from './components/NewsLists/NewsLists';
-import { newsDataFetch } from './util/NewsDataFetch';
+import SelectLists from './components/SelectLists/SelectLists';
+import SideNewsLists from './components/SideNewsLists/SideNewsLists';
+
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      articles:[],
-      loading:true
-    }
-  }
-  componentDidMount(){
-    setTimeout(()=>{
-      newsDataFetch().then(data=>this.setState({
-        articles:data,
-        loading:false
-        })
-      )
-    },1000);
-
-
-  }
 
   render() {
+
     return (
       <div className="App">
-        <NewsLists newsfeed={this.state.articles} loading={this.state.loading}/>
+        <SelectLists />
+        <NewsLists />
+        <SideNewsLists />
       </div>
     );
   }
